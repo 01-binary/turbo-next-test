1844bb4a564c0c855b05801b4e4d2e501bbee564
jinsoo <binary01@kakao.com>
2022-10-12 14:44:07 +0900

    feat: auto change

--- apps/auto/pages/index.tsx
+++ apps/auto/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "components";
 export default function Main() {
   return (
     <div>
-      <h1>Auto Main</h1>
+      <h1>Auto Main test1</h1>
       <Button />
     </div>
   );
