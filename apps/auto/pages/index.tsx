1b7bef515b68cb4dd4f4bb4823c1a54c94b58bb5
jinsoo <binary01@kakao.com>
2022-10-12 18:00:00 +0900

    yml modfiy

--- apps/auto/pages/index.tsx
+++ apps/auto/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "components";
 export default function Main() {
   return (
     <div>
-      <h1>Auto Main test2</h1>
+      <h1>Auto Main test3</h1>
       <Button />
     </div>
   );
