27644c284dcc96fca18bba853b383d48d367d629
jinsoo <jinsoo@finda.co.kr>
2022-10-13 13:01:35 +0900

    바꿈

--- apps/auto/pages/index.tsx
+++ apps/auto/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "components";
 export default function Main() {
   return (
     <div>
-      <h1>Auto Main test4</h1>
+      <h1>Auto Main 바꿧다.</h1>
       <Button />
     </div>
   );
