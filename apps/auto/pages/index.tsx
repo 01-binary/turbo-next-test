dcdca1a9c0c778f54e1b9b8ceaba832d92f9ec37
jinsoo <admin@binary01.me>
2023-05-15 01:45:53 +0900

    asd

--- apps/auto/pages/index.tsx
+++ apps/auto/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "components";
 export default function Main() {
   return (
     <div>
-      <h1>Auto Main 바꿧다.2</h1>
+      <h1>Auto Main 바꿧다.3</h1>
       <Button />
     </div>
   );
