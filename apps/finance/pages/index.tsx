43ab0702a499b3f18843440b8b0e82ae46ee99e5
jinsoo <admin@binary01.me>
2023-05-14 22:14:08 +0900

    test

--- apps/finance/pages/index.tsx
+++ apps/finance/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "components";
 export default function Main() {
   return (
     <div>
-      <h1>Finance Main1</h1>
+      <h1>Finance Main2</h1>
       <Button />
     </div>
   );
